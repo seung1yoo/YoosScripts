72b471d0a18ec5735e22639e7acb30eedbe9314e
Seung-il Yoo <Yoo@Seung-ilui-MacBook-Air.local>
2017-01-13 12:48:20 +0900

    HeatmapMulti_forItself.R update

--- Rscripts/Heatmap/HeatmapMulti_forItself.R
+++ Rscripts/Heatmap/HeatmapMulti_forItself.R
@@ -20,6 +20,17 @@ head(data)
 rownames(data)
 colnames(data)
 
+#column names change
+colnames(data)<-c("HighVirulence_Control_1d.vs.HighVirulence_Virus_1d","HighVirulence_Control_3d.vs.HighVirulence_Virus_3d",
+            "HighVirulence_Control_1w.vs.HighVirulence_Virus_1w","LowVirulence_Control_1d.vs.LowVirulence_Virus_1d",
+            "LowVirulence_Control_3d.vs.LowVirulence_Virus_3d","LowVirulence_Control_1w.vs.LowVirulence_Virus_1w")
+
+#extracting specific columns from a data frame
+colnames(data)
+colnames(data[,c(1,2,3,7,8,9)])
+data <- data[,c(1,2,3,7,8,9)]
+colnames(data)
+head(data,1)
 
 #Create fake color side bars
 ##rownames(data)
@@ -38,9 +49,9 @@ colnames(data)
 #clab=cbind(subtype_colors,Mcolors,Ncolors,Tcolors,HER2colors,PRcolors,ERcolors)
 #colnames(clab)=c("Subtype","M","N","T","HER2","PR","ER")
 colnames(data)
-timeColors=c("lightpink1","lightpink2","lightpink3","lightpink1","lightpink2","lightpink3","lightpink1","lightpink2","lightpink3")
-treatColors=c("darkorchid1","darkorchid1","darkorchid1","darkorchid3","darkorchid3","darkorchid3","darkorchid1","darkorchid1","darkorchid1")
-virulenceColors=c('gold2','gold2','gold2','gold3','gold3','gold3','gold4','gold4','gold4')
+timeColors=c("lightpink1","lightpink2","lightpink3","lightpink1","lightpink2","lightpink3")
+treatColors=c("darkorchid1","darkorchid1","darkorchid1","darkorchid1","darkorchid1","darkorchid1")
+virulenceColors=c('gold2','gold2','gold2','gold4','gold4','gold4')
 clab=cbind(timeColors,treatColors,virulenceColors)
 colnames(clab)=c("TimeSeries Comparison","Treatment Comparison","Virulence Comparison")
 colnames(clab)
@@ -66,15 +77,19 @@ for(i in 1:length(cluster.method.list)){
 main_title="DEG of Virulence pairs"
 mydist=function(c) {dist(c,method="minkowski")}
 myclust=function(c) {hclust(c,method="complete")}
+myPalette <- colorRampPalette(rev(brewer.pal(5, "RdBu")))
 par(cex.main=1)
 heatmap.3(data, hclustfun=myclust, distfun=mydist, na.rm = TRUE, scale="row", dendrogram="both", margins=c(30,10),
-          Rowv=TRUE, Colv=TRUE, ColSideColors=clab, symbreaks=FALSE, key=TRUE, symkey=FALSE,
-          density.info="none", trace="none", main=main_title, labCol=colnames(data), labRow=FALSE, cexRow=1, col=myPalette,
-          ColSideColorsSize=3, RowSideColorsSize=2, KeyValueName=FALSE)
-legend("topright",
-       legend=c("Highly virulence","Low virulence","","Control","virus","","1 day","3 day","1 week"),
-       fill=c("black","grey","white","pink","cyan","white","red","blue","green"), 
-       border=TRUE, bty="n", y.intersp = 0.9, cex=0.9)
+          Rowv=TRUE, Colv=TRUE, ColSideColors=clab, symbreaks=FALSE, 
+          key=TRUE, keysize = 1, symkey=FALSE,
+          density.info="none", trace="none", main=main_title, labCol=colnames(data), labRow=FALSE, cexRow=1, cexCol=1.3, 
+          col=myPalette, ColSideColorsSize=3, RowSideColorsSize=2, KeyValueName=FALSE)
+# legend : Specify legend position by keywords
+# http://www.sthda.com/english/wiki/add-legends-to-plots-in-r-software-the-easiest-way
+legend("topright", inset = .02,
+       legend=c("High vs High","Low vs Low","","Control vs virus     ","","1d vs 1d","3d vs 3d","1w vs 1w"),
+       fill=c("gold2","gold4","white","darkorchid1","white","lightpink1","lightpink2","lightpink3"), 
+       border=FALSE, bty="n", angle = 90, y.intersp = 1, cex=0.7)
 
 #legend=c("Basal","LumA","LumB","Her2","Claudin","Normal","","Positive","Negative","NA","","Targeted","Chemo","","Approved","Experimental"),
 #fill=c("red","blue","cyan","pink","yellow","green","white","black","white","grey","white","darkorchid","darkred","white","green","darkgreen"), 
