82599dd4d1d7afde49ccb0c57562d186d940cf58
Seung-il Yoo <Yoo@Seung-ilui-MacBook-Air.local>
2017-03-27 19:25:31 +0900

    execute tissue specific gene

---
+++ Tissue-specific-genes/execute.sh
@@ -0,0 +1,6 @@
+python deg_parser_ee_siyoo.py \
+    -d tissue_specific_input.xls \
+    -a gene_transcript_info.anno.xls \
+    -o tissue_specific_output.xls \
+    -e 4.0 -f 0.3 -p 1e-3 \
+    -t Flower Stem Root Leaf
