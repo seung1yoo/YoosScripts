e3284205c236bb6bd7d72547163194502f52a130
Seung-il Yoo <Yoo@Seung-ilui-MacBook-Air.local>
2017-03-28 17:48:45 +0900

    new example for tissue_specific_gene

--- Tissue-specific-genes/execute.sh
+++ Tissue-specific-genes/execute.sh
@@ -2,5 +2,5 @@ python deg_parser_ee_siyoo.py \
     -d tissue_specific_input.xls \
     -a gene_transcript_info.anno.xls \
     -o tissue_specific_output.xls \
-    -e 4.0 -f 0.3 -p 1e-3 \
+    -e 2.0 -f 0.3 -p 0.05 \
     -t Flower Stem Root Leaf
