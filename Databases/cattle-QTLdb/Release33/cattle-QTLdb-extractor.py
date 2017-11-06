0b89bfd0e9183bfad3cbd74b844a4266e7e42403
Seung-il Yoo <Yoo@Seung-ilui-MacBook-Air.local>
2017-11-06 16:33:34 +0900

    modify cattle-QTLdb-extractor

--- Databases/cattle-QTLdb/Release33/cattle-QTLdb-extractor.py
+++ Databases/cattle-QTLdb/Release33/cattle-QTLdb-extractor.py
@@ -6,13 +6,13 @@ def term_checker(category, termDic):
             return 1
     return 0
 
-def qtl_parser(originBED, listFile, trait, sub_trait):
+def qtl_parser(originBED, listFile, trait, sub_trait, release):
     termDic = dict()
     for line in open(listFile):
         termDic.setdefault(line.strip(), '')
     #
     cateDic = dict()
-    out = open('cattle-QTLdb.Release33.{0}.{1}.bed'.format(trait, sub_trait), 'w')
+    out = open('cattle-QTLdb.{0}.{1}.{2}.bed'.format(release, trait, sub_trait), 'w')
     for line in open(originBED):
         if line.startswith('#'):
             continue
@@ -33,27 +33,29 @@ def qtl_parser(originBED, listFile, trait, sub_trait):
         cateDic.setdefault(category, {}).setdefault(name, 0)
         cateDic[category][name] += 1
     out.close()
-    cmd = 'bedtools sort -i cattle-QTLdb.Release33.{0}.{1}.bed > cattle-QTLdb.Release33.{0}.{1}.sort.bed'.format(trait, sub_trait)
+    cmd = 'bedtools sort -i cattle-QTLdb.{0}.{1}.{2}.bed > cattle-QTLdb.{0}.{1}.{2}.sort.bed'.format(release, trait, sub_trait)
     print cmd
     os.system(cmd)
     #
-    out = open('cattle-QTLdb.Release33.{0}.{1}.stats.xls'.format(trait, sub_trait), 'w')
+    out = open('cattle-QTLdb.{0}.{1}.{2}.stats.xls'.format(release, trait, sub_trait), 'w')
     for category, nameDic in cateDic.iteritems():
         if term_checker(category, termDic) and category.endswith('QTL'):
             out.write('{0}\n'.format('\t'.join([category, '@', str(len(nameDic))])))
     out.close()
     #
 
-def main():
-    originBED = 'cattle-QTLdb.Release33.bed'
-    # #
+def main(args):
+    
+    qtl_parser(args.originBED, args.traitListFn, args.trait, args.sub_trait, args.release)
+
+    # # Regacy command for DEV.
     #qtl_parser(originBED, 'Reproduction_Traits.All.QTLterms', 'Reproduction', 'All')
     #qtl_parser(originBED, 'Reproduction_Traits.Fertility.QTLterms', 'Reproduction', 'Fertility')
     #qtl_parser(originBED, 'Reproduction_Traits.General.QTLterms', 'Reproduction', 'General')
     #qtl_parser(originBED, 'Reproduction_Traits.Reproductive-hormone-level.QTLterms', 'Reproduction', 'Reproductive-hormone-level')
     #qtl_parser(originBED, 'Reproduction_Traits.Semen-quality.QTLterms', 'Reproduction', 'Semen-quality')
     # #
-    qtl_parser(originBED, 'Milk_Traits.All.QTLterms', 'Milk', 'All')
+    #qtl_parser(originBED, 'Milk_Traits.All.QTLterms', 'Milk', 'All')
     #qtl_parser(originBED, 'Milk_Traits.Milk_composition_fat.QTLterms', 'Milk', 'Milk_composition_fat')
     #qtl_parser(originBED, 'Milk_Traits.Milk_composition_other.QTLterms', 'Milk', 'Milk_composition_other')
     #qtl_parser(originBED, 'Milk_Traits.Milk_composition_protein.QTLterms', 'Milk', 'Milk_composition_protein')
@@ -62,4 +64,17 @@ def main():
     # #
 
 if __name__=='__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ob', '--originBED', help='cattle QTL information as BED format',
+            default='cattle-QTLdb.Release33.bed')
+    parser.add_argument('-tl', '--traitListFn', help='trait list file name',
+            default='Milk_Traits.All.QTLterms')
+    parser.add_argument('-r', '--release', default='Release33')
+    parser.add_argument('-t', '--trait', help='Trait category',
+            default='Milk')
+    parser.add_argument('-st', '--sub-trait', help='Sub-Trait name',
+            default='All')
+    args = parser.parse_args()
+    main(args)
+
