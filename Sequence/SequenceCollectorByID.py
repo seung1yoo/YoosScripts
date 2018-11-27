deb63658aa32cc4e1b678bf445e7f931895604bb
seung1.yoo <seung1.yoo@gmail.com>
2018-11-27 11:08:19 +0900

    modified SequenceCollectorByID.py

--- Sequence/SequenceCollectorByID.py
+++ Sequence/SequenceCollectorByID.py
@@ -1,28 +1,66 @@
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 
-def listDicMaker(idListFile):
-    idDic = dict()
-    for line in open(idListFile):
-        items = line.rstrip('\n').split('\t')
-        idDic.setdefault(items[0])
-    return idDic
+class SEQUENCE:
+    def __init__(self):
+        self.seqDic = dict()
 
-def main(args):
-    print(args)
-    idDic = listDicMaker(args.idList)
+    def get_list(self, listFn):
+        for line in open(listFn):
+            gene_id = line.rstrip('\n')
+            if gene_id:
+                self.seqDic.setdefault(gene_id, {})
+            else:
+                continue
+            #
+        #
 
-    out = open(args.output, 'w')
-    for record in SeqIO.parse(open(args.input), args.inputType):
-        if record.id in idDic:
-            SeqIO.write(record, out, args.inputType)
-    out.close()
+    def get_seq(self, faFn, faKey):
+        for record in SeqIO.parse(open(faFn), 'fasta'):
+            gene_id = record.id.split('|')[1]
+            if gene_id in self.seqDic:
+                self.seqDic[gene_id].setdefault(faKey, record.seq)
+            else:
+                continue
+            #
+        #
+
+    def make_fa(self, outPrefix, faKey):
+        out = open('{0}.{1}.fa'.format(outPrefix, faKey),'w')
+        for gene_id, faKeyDic in self.seqDic.iteritems():
+            seq = faKeyDic[faKey]
+            record = SeqRecord(seq, id=gene_id, description="")
+            SeqIO.write(record, out, 'fasta')
+        out.close()
+
+    def make_table(self, outPrefix, *faKeys):
+        out = open('{0}.table.xls'.format(outPrefix), 'w')
+        out.write('gene_id\t{0}\n'.format('\t'.join(faKeys)))
+        for gene_id, faKeyDic in self.seqDic.iteritems():
+            items = [gene_id]
+            items.extend([str(faKeyDic[x]) for x in faKeys])
+            out.write('{0}\n'.format('\t'.join(items)))
+        out.close()
+
+
+def main():
+    #
+    listFn = 'list.txt'
+    cdsfa = 'haliotis_0.1.CDS'
+    pepfa = 'haliotis_0.1.protein'
+    outPrefix = 'SelectedSeq'
+    #
+    seq = SEQUENCE()
+    #
+    seq.get_list(listFn)
+    seq.get_seq(cdsfa, 'cds')
+    seq.get_seq(pepfa, 'pep')
+    #
+    seq.make_fa(outPrefix, 'cds')
+    seq.make_fa(outPrefix, 'pep')
+    seq.make_table(outPrefix, 'cds', 'pep')
+
+
 
 if __name__=='__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input')
-    parser.add_argument('-l', '--idList')
-    parser.add_argument('-t', '--inputType', choices=('fastq','fasta'))
-    parser.add_argument('-o', '--output')
-    args = parser.parse_args()
-    main(args)
+    main()
