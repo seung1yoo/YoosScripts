06cb03c334e23096475f6de1036abe0c9a6d4336
Seung-il Yoo <seung1.yoo@gmail.com>
2020-01-09 15:37:29 +0900

    update setinfo

---
+++ tbi-service/SequenceInfo/set2/command.sh
@@ -0,0 +1,2 @@
+python2 FastqStatExtractFastqGZ.pairedend.py sample__1_-3ld-2_1.fq.gz sample__1_-3ld-2_2.fq.gz sample__1_-3ld-2_1.fq.gz.rst.xls
+python sumuprst.py outprefix.sumuprst.json Sequencing_Statistics_Result.xls --header T
